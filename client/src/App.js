acadbd72cef4252d3faf96724ac49db94b8ed0c1
Billy Risigo <wlrisigo@gmail.com>
2018-12-09 01:50:00 -0500

    working again

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import AboutMe from './Pages/about-me.jsx';
+import News from './Pages/news.jsx';
+import ViewDogs from './Pages/view-dogs';
+import {Route,Switch} from 'react-router-dom';
+import AddDog from "./Pages/addDog";
+import AddUser from "./Pages/addUser";
+
+class App extends Component {
+    render() {
+        const App = () => (
+
+        <div>
+        <Switch>
+                <Route exact={true} path={'/'} component={News}/>
+                <Route exact={true} path={'/news'} component={News}/>
+                <Route exact={true} path={'/AboutUs'} component={AboutMe}/>
+                <Route exact={true} path={'/OurDogs'} component={ViewDogs}/>
+                <Route exact={true} path={'/addDog'} component={AddDog}/>
+            <Route exact={true} path={'/addUser'} component={AddUser}/>
+
+
+        </Switch>
+        </div>
+    );
+        return (
+            <Switch>
+                <App/>
+            </Switch>
+        );
+    }
+}
+
+export default App;
---
+++ client/src/components/nav.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+const css = 'Nav';
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <nav className={css}>
+                <a className={css} href='News'>News</a>
+                <a className={css} href='AboutUs'>About Us</a>
+                <a className={css} href='OurDogs'>View our Dogs</a>
+                <a className={css} href='addDog'>Add a dog</a>
+                <a className={css} href='addUser'>Add a User</a>
+            </nav>
+        );
+    }
+}
+
+
+
+
